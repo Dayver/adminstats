c0499e293d6bc3bbb9fb33e9482d94ab90f9fae7
seditio <support@seditio.by>
2013-10-09 07:32:48 +0300

    fix lang strings

--- lang/adminstats.ru.lang.php
+++ lang/adminstats.ru.lang.php
@@ -12,31 +12,35 @@ $L['info_desc'] = 'Детализированная статистика, дос
  * Plugin Config
  */
 
-$L['cfg_cache_ttl'] = 'Время жизни кеша в секундах';
-$L['cfg_cache_ttl_hint'] = '0 - кеш отключен';
+$L['cfg_cache_ttl'] = 'Время жизни кэша в секундах';
+$L['cfg_cache_ttl_hint'] = '0 - кэш отключен';
 
 /**
  * Plugin Admin
  */
+
 $L['adminstats_cot_version'] = &$L['Version'];
-$L['adminstats_cot_defaulttheme'] = 'Тема оформления, по умолчанию';
-$L['adminstats_cot_defaultscheme'] = 'Цветовая схема, по умолчанию';
-$L['adminstats_cot_defaulticons'] = 'Пакет значков, по умолчанию';
-$L['adminstats_cot_defaultlang'] = 'Язык, по умолчанию';
-$L['adminstats_cot_admintheme'] = 'Тема оформления панели администрирования';
+
+$L['adminstats_cot_defaulttheme'] = 'Тема оформления';
+$L['adminstats_cot_defaultscheme'] = 'Цветовая схема';
+$L['adminstats_cot_defaulticons'] = 'Иконки';
+$L['adminstats_cot_defaultlang'] = 'Язык';
+$L['adminstats_cot_admintheme'] = 'Тема админ-панели';
+
 $L['adminstats_cot_cache'] = 'Кэширование данных';
 $L['adminstats_cot_cache_drv'] = 'Кэш драйвер';
 $L['adminstats_cot_cache_drv_Empty']= &$L['Gender_U'];
 $L['adminstats_cot_xtpl_cache'] = 'Кэширование шаблонов';
-$L['adminstats_cot_html_cleanup'] = 'Удаление пробелов и переносов в HTML';
-$L['adminstats_cot_cache_index'] = 'Статическая кэш для гостей главной';
-$L['adminstats_cot_cache_page'] = 'Статическая кэш для гостей страниц и категорий';
-$L['adminstats_cot_cache_forums'] = 'Статическая кэш для гостей на форумах';
-$L['adminstats_cot_check_updates'] = 'Автоматически проверять наличие обновлений';
+$L['adminstats_cot_html_cleanup'] = 'Компактная HTML-сигнатура';
+$L['adminstats_cot_cache_index'] = 'Статический кэш для главной страницы';
+$L['adminstats_cot_cache_page'] = 'Статический кэш для страниц и разделов';
+$L['adminstats_cot_cache_forums'] = 'Статический кэш для форумов';
+
+$L['adminstats_cot_check_updates'] = 'Проверка обновлений';
 $L['adminstats_cot_display_errors'] = 'Отображения сообщений об ошибках';
 $L['adminstats_cot_ipcheck'] = 'Проверка IP при авторизации';
 $L['adminstats_cot_authcache'] = 'Кэширование прав в SQL таблицы БД';
-$L['adminstats_cot_useremailduplicate'] = 'Регистрация новых учетные записи с дублирующимся Email';
+$L['adminstats_cot_useremailduplicate'] = 'Разрешить учетные записи с дублирующимися Email';
 $L['adminstats_cot_debug_mode'] = 'Режим отладки';
 
 $L['adminstats_modules'] = &$L['Modules'];
@@ -48,7 +52,6 @@ $L['adminstats_active_hooks'] = 'Активные хуки';
 
 $L['adminstats_cot_database'] = &$L['Database'];
 $L['adminstats_mysqlcharset'] = 'Кодировка БД SQL';
-
 $L['adminstats_db_counts'] = 'БД SQL, таблиц';
 $L['adminstats_db_rows'] = &$L['home_db_rows'];
 $L['adminstats_db_indexsize'] = &$L['home_db_indexsize'];
@@ -68,9 +71,3 @@ $L['adminstats_new_ver_msg'] = 'Анонс новой версии';
 
 $L['adminstats_error_type_disabled'] = 'Расширение %1$s не активно';
 $L['adminstats_error_type_wrong'] = 'Указанно но поддерживаемое расширение';
-
-/**
- * Plugin Admin
- */
-
-//$L['adminstats_'] = '';
